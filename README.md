e9c615e8480ec9daca024e893cf3994634546d49
Kay Schecker <sayhello@kay-schecker.de>
2017-12-28 18:54:15 +0100

    added/fixed coverage reports

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # @harmowatch/redux-decorators
 
 [![Build Status](https://travis-ci.org/HarmoWatch/redux-decorators.svg?branch=master)](https://travis-ci.org/HarmoWatch/redux-decorators)
+[![Coverage Status](https://coveralls.io/repos/github/HarmoWatch/redux-decorators/badge.svg?branch=master)](https://coveralls.io/github/HarmoWatch/redux-decorators?branch=master)
--- karma.conf.js
+++ karma.conf.js
@@ -1,49 +1,27 @@
-var webpackConf = require('./webpack.config.js');
 module.exports = function (config) {
 
     config.set({
-        basePath: '',
-        frameworks: ['jasmine'],
-        files: [{
-            pattern: './tests/unit/spec-bundle.js',
-            watched: false
-        }],
+        frameworks: ['jasmine', 'karma-typescript'],
+        files: [
+            'src/**/*.ts' // *.tsx for React Jsx
+        ],
         preprocessors: {
-            './tests/unit/spec-bundle.js': ['coverage', 'webpack', 'sourcemap']
+            '**/*.ts': 'karma-typescript' // *.tsx for React Jsx
         },
-        webpack: {
-            module: webpackConf.module,
-            resolve: webpackConf.resolve
-        },
-        webpackMiddleware: {
-            noInfo: true,
-            stats: 'errors-only'
-        },
-        reporters: ['kjhtml', 'spec', 'coverage'],
-        // optionally, configure the reporter
-        coverageReporter: {
-            // specify a common output directory
-            dir: './tests/build/reports/coverage',
-            reporters: [
-                // reporters not supporting the `file` property
-                {type: 'html', subdir: 'report-html'},
-                {type: 'lcov', subdir: 'report-lcov'},
-                // reporters supporting the `file` property, use `subdir` to directly
-                // output them in the `dir` directory
-                {type: 'cobertura', subdir: '.', file: 'cobertura.txt'},
-                {type: 'lcovonly', subdir: '.', file: 'report-lcovonly.txt'},
-                {type: 'teamcity', subdir: '.', file: 'teamcity.txt'},
-                {type: 'text', subdir: '.', file: 'text.txt'},
-                {type: 'text-summary', subdir: '.', file: 'text-summary.txt'},
-            ]
-        },
-        specReporter: {
-            maxLogLines: 5,         // limit number of lines logged per test
-            suppressErrorSummary: true,  // do not print error summary
-            suppressFailed: false,  // do not print information about failed tests
-            suppressPassed: false,  // do not print information about passed tests
-            suppressSkipped: true,  // do not print information about skipped tests
-            showSpecTiming: false // print the time elapsed for each spec
+        reporters: ['progress', 'karma-typescript'],
+        karmaTypescriptConfig: {
+            tsconfig: 'tsconfig.json',
+            reports: {
+                html: {
+                    "directory": "./",
+                    "subdirectory": "coverage"
+                },
+                "lcovonly": {
+                    "directory": "./",
+                    "filename": "coverage.lcov",
+                    "subdirectory": "coverage"
+                },
+            },
         },
         customLaunchers: {
             Chrome_travis_ci: {
@@ -54,45 +32,12 @@ module.exports = function (config) {
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
-        autoWatch: true,
         browsers: ['Chrome'],
         singleRun: true,
         concurrency: Infinity
     });
 
-
     if (process.env.TRAVIS) {
         config.browsers = ['Chrome_travis_ci'];
     }
 };
-//
-// module.exports = function (config) {
-//     config.set({
-//         basePath: '',
-//         frameworks: ['jasmine'],
-//         plugins: [
-//             require('karma-jasmine'),
-//             require('karma-chrome-launcher'),
-//             require('karma-jasmine-html-reporter'),
-//             require('karma-coverage-istanbul-reporter'),
-//             require('@angular/cli/plugins/karma')
-//         ],
-//         client:{
-//             clearContext: false // leave Jasmine Spec Runner output visible in browser
-//         },
-//         coverageIstanbulReporter: {
-//             reports: [ 'html', 'lcovonly' ],
-//             fixWebpackSourcePaths: true
-//         },
-//         angularCli: {
-//             environment: 'dev'
-//         },
-//         reporters: ['progress', 'kjhtml'],
-//         port: 9876,
-//         colors: true,
-//         logLevel: config.LOG_INFO,
-//         autoWatch: true,
-//         browsers: ['Chrome'],
-//         singleRun: false
-//     });
-// };
