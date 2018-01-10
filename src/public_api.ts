6501ade60591f9d5bf2adb993e8a837adaad4b3c
Kay Schecker <sayhello@kay-schecker.de>
2018-01-10 21:24:52 +0100

    updated public api

--- src/public_api.ts
+++ src/public_api.ts
@@ -1,6 +1,19 @@
-export * from './action/context/decorator/redux-action-context-decorator';
+// action
 export * from './action/decorator/redux-action-decorator';
 export * from './action/dispatcher/redux-action-dispatcher';
+export * from './action/function/redux-action-function.type';
+
+// action context
+export * from './action/context/decorator/redux-action-context-decorator';
+
+// module
 export * from './module/decorator/redux-module-decorator';
+
+// reducer
 export * from './reducer/decorator/redux-reducer-decorator';
+export * from './reducer/redux-reducer.type';
+
+// state
 export * from './state/decorator/redux-state-decorator';
+export * from './state/redux-state.interface';
+export * from './state/redux-state.type';
